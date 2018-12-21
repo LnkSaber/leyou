0351ba62bb14ae5d31d88003074ea72db2a75452
pc <198910310@qq.com>
2018-12-21 08:45:01 +0800

    备份

---
+++ ly-page/src/main/java/com/leyou/page/mq/ItemListener.java
@@ -0,0 +1,48 @@
+package com.leyou.page.mq;
+
+
+import com.leyou.page.service.PageService;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ItemListener {
+    @Autowired
+    private PageService pageService;
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "page.item.insert.queue",durable = "true"),
+            exchange = @Exchange(name = "ly.item.exchange",type = ExchangeTypes.TOPIC),
+            key = {"item.insert","item.update"}
+    ))
+    public void ListenInsertOrUpdate(Long spuId){
+        if (spuId ==null){
+            return;
+        }
+        //处理消息，创建静态页
+        pageService.createHtml(spuId);
+
+    }
+
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "page.item.delete.queue",durable = "true"),
+            exchange = @Exchange(name = "ly.item.exchange",type = ExchangeTypes.TOPIC),
+            key = {"item.delete"}
+    ))
+    public void ListenDelete(Long spuId){
+        if (spuId ==null){
+            return;
+        }
+        //处理消息，对静态页进行删除
+        pageService.deleteHtml(spuId);
+
+    }
+
+
+}
